8bcf6741fc170b79c8d2cce8a288862b5ee8aee2
Bartoszcz28 <bartoszczarnecki26@gmail.com>
2024-04-02 17:44:42 +0200

    jdk8

--- java.sh
+++ java.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
-# Upgrade & installation
-# sudo apt-get update && sudo apt-get upgrade -y
+# # Changing directory
+# cd /opt
+# # Java download
+# sudo wget https://cdn.azul.com/zulu/bin/zulu21.32.17-ca-jdk21.0.2-linux_aarch64.tar.gz
+# # Java Unpacking
+# sudo tar -zxvf zulu21.32.17-ca-jdk21.0.2-linux_aarch64.tar.gz
+# # Removal of unnecessary files
+# sudo rm zulu21.32.17-ca-jdk21.0.2-linux_aarch64.tar.gz
+# # Name change
+# sudo mv zulu21.32.17-ca-jdk21.0.2-linux_aarch64 jdk21
+# # Installation 
+# sudo update-alternatives --install /usr/bin/java java /opt/jdk21/bin/java 1
+# # Selecting configurations
+# # sudo update-alternatives --config java -1
+# # Adding environment variables
+# echo 'export JAVA_HOME=/opt/jdk21' >> ~/.bashrc
+# # Update of the current session
+# source ~/.bashrc
+
+# java -version
+
 
 # Changing directory
 cd /opt
+# Library required by java
+sudo apt-get install -y musl 
 # Java download
-sudo wget https://cdn.azul.com/zulu/bin/zulu21.32.17-ca-jdk21.0.2-linux_aarch64.tar.gz
+sudo wget https://cdn.azul.com/zulu/bin/zulu8.72.0.17-ca-jdk8.0.382-linux_musl_aarch64.tar.gz
 # Java Unpacking
-sudo tar -zxvf zulu21.32.17-ca-jdk21.0.2-linux_aarch64.tar.gz
+sudo tar -zxvf zulu8.72.0.17-ca-jdk8.0.382-linux_musl_aarch64.tar.gz
 # Removal of unnecessary files
-sudo rm zulu21.32.17-ca-jdk21.0.2-linux_aarch64.tar.gz
+sudo rm zulu8.72.0.17-ca-jdk8.0.382-linux_musl_aarch64.tar.gz
 # Name change
-sudo mv pyspark-3.5.1 spark
+sudo mv zulu8.72.0.17-ca-jdk8.0.382-linux_musl_aarch64 jdk8
 # Installation 
-sudo update-alternatives --install /usr/bin/java java /opt/zulu21.32.17-ca-jdk21.0.2-linux_aarch64/bin/java 1
-# Selecting configurations
-# sudo update-alternatives --config java -1
+sudo update-alternatives --install /usr/bin/java java /opt/jdk8/bin/java 1
 # Adding environment variables
-echo 'export JAVA_HOME=/opt/zulu21.32.17-ca-jdk21.0.2-linux_aarch64' >> ~/.bashrc
+echo 'export JAVA_HOME=/opt/jdk8' >> ~/.bashrc
 # Update of the current session
 source ~/.bashrc
 
-# java -version
+sudo apt-get update && sudo apt-get upgrade -y
